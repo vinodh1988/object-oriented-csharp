b562b1a284ca47dced5c54bf48dca4380bb08ff2
vinodh1988 <k.c.vinodh@hotmail.com>
2022-10-28 10:55:28 +0530

    first-commit

---
+++ ObjectOriented/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOriented
+{
+    /*
+     * Wrapping up of data and functionalities together under one unit
+     * is called Encapsulation
+     * 
+     * In OOP, a class is a template[model]
+     * 
+     * to realize the template[model] we need to instantiate it
+     * 
+     * the syntax to instantiate objects in c# is 
+     * 
+     *  new Person();
+     *  
+     *  here Person() is called an constructor
+     *  
+     *  A constructor is a special method which has same name as that of the class
+     *  
+     *  it is used to instantiate a class
+     *  
+     *  each instance occupies a seperate memory space
+     *  
+     *  In the Person class case,
+     *  
+     *   sno,name,city are not accessible with instance reference
+     *   Show() is accessible
+     *   
+     *   It is the Class creators responsibility to allow only
+     *   the essential features and hide the key background details
+     *   and implementations
+     *   
+     *   we achieve with the help of access modifiers
+     *   
+     *   Abstraction - allow essential features hide background implementation
+     *   
+     *   Polymorphism - One name many forms
+     *   
+     *              1. Method overloading
+     *              2. Constructor overloading
+     *              3. overriding
+     * */
+    internal class Person
+    {
+        private int sno;
+        private string name;
+        private string city;
+        
+        public Person() { }
+        public Person(int sno, string name, string city) {
+            this.sno = sno;
+            this.name = name;
+            this.city = city;
+        }
+    public void Show() {
+            Console.WriteLine("Sno: {0}", sno);
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("City: {0}", city);
+        }
+
+        public void SetValues(int sno, string name, string city) {
+            this.sno = sno;
+            this.name = name;
+            this.city = city;
+        }
+
+        
+    }
+}
---
+++ ObjectOriented/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectOriented
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Person p,q; //reference variable
+
+            p=new Person(); //object assigned to reference variable
+            q = new Person(1,"Rakesh","Pune");
+           // p.sno = 20; //error
+            p.Show();
+            Console.WriteLine("q print");
+            q.Show();
+            p.SetValues(1, "Ravi", "Mumbai");
+            q.SetValues(2, "Ramesh", "Chennai");
+            p.Show();
+            Console.WriteLine("q print");
+            q.Show();
+        }
+    }
+}
+
+/*
+ * A language can be categorized into object oriented programming language
+ * if and only if it support 4 features
+ * 
+ * 1. Encapsulation
+ * 2. Abstraction
+ * 3. Polymorphism
+ * 4. Inherita ce
+ * 
+ */ 
